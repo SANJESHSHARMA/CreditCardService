07b617f2ba9fa88e7cdd701a22c8f7569caae297
Sanjesh Sharma <sanjeshsharma.99@gmail.com>
2025-04-24 20:39:30 +0530

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/CreditCardService/CreditCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CreditCardService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
